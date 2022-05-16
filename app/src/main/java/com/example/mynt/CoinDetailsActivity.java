6fa61e5f51143898dc230407725d22b573550db6
JoshMkhari <njmkhari@gmail.com>
2022-05-16 11:11:27 +0200

    Coin Details view

---
+++ app/src/main/java/com/example/mynt/CoinDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.mynt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoinDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_details);
+    }
+}
