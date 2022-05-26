89b6d146a9304a1a94d08b0d2633555b0f320be7
Mkhuseli Ngetu <105724548+MkhuseliNgetu@users.noreply.github.com>
2022-05-26 18:58:07 +0200

    (Fragment_Library methods fix)

--- app/src/main/java/com/example/mynt/collectionsActivity/Fragment_Library.java
+++ app/src/main/java/com/example/mynt/collectionsActivity/Fragment_Library.java
@@ -128,7 +128,10 @@ public class Fragment_Library extends Fragment implements RecyclerViewInterface 
 
 
         ReturnToRegister();
+        ViewLoggedInUser();
         NavigationToOtherPages();
+        DisplayAllLocalCollections();
+        DisplayAllLocalCoins();
 
         return libraryView;
     }
