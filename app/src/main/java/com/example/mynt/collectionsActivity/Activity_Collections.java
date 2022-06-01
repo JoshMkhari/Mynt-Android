be6c18f2b4d7e6f0b3d5f014e1550a816e639f66
JoshMkhari <njmkhari@gmail.com>
2022-06-01 22:13:13 +0200

    attempt to splash screen

--- app/src/main/java/com/example/mynt/collectionsActivity/Activity_Collections.java
+++ app/src/main/java/com/example/mynt/collectionsActivity/Activity_Collections.java
@@ -6,18 +6,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 import com.example.mynt.R;
 
 import java.util.Objects;
 
+import pl.droidsonroids.gif.GifTextView;
+
 public class Activity_Collections extends AppCompatActivity {
+    private GifTextView gifImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collections);
 
+
+        gifImageView = findViewById(R.id.imageViewGIf);
         Bundle bundle = getIntent().getExtras();
         int page = 1;
         if(bundle == null)
@@ -31,9 +41,45 @@ public class Activity_Collections extends AppCompatActivity {
             bundle.putInt("StartPage",page);
         }
 
-        Log.d("EasyPeasyt","runing "+ page);
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                gifImageView.setBackgroundResource(R.drawable.splash_screen);
+                //Intent mainIntent = new Intent(SplashActivity.this,LoginActivity.class);
+                //SplashActivity.this.startActivity(mainIntent);
+                //SplashActivity.this.finish();
+            }
+        }, 2500);
 
+        Log.d("EasyPeasyt","runing "+ page);
+        //startSplash();
         findNavController(Objects.requireNonNull(getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2))).
                 setGraph(R.navigation.collection_navigation,bundle);
+    }
+
+    public void startSplash(){
+        Animation fadeout = new AlphaAnimation(1.f, 1.f);
+        fadeout.setDuration(2500);
+        final View viewToAnimate = gifImageView;
+        fadeout.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+                gifImageView.setBackgroundResource(R.drawable.splash_screen);//splash_screen is your .gif file
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+        //gifImageView.startAnimation(fadeout);
+        });
     }
 }
