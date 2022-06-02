53e26582cfd101d3b338a432cc8cb632dbdb5722
JoshMkhari <njmkhari@gmail.com>
2022-06-02 21:14:27 +0200

    Fixing navigation indicators

--- app/src/main/java/com/example/mynt/collectionsActivity/Fragment_home_main.java
+++ app/src/main/java/com/example/mynt/collectionsActivity/Fragment_home_main.java
@@ -3,16 +3,19 @@ package com.example.mynt.collectionsActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.mynt.Activity_Main;
@@ -24,6 +27,7 @@ import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,16 +38,17 @@ public class Fragment_home_main extends Fragment {
     Model_User user = new Model_User();
     private LinearLayout layoutIndicators;
     private Database_Lite db;
-
+    private Adapter_HomeActFragment fragmentAdapter;
+    View home;
     //23:54
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View home = inflater.inflate(R.layout.fragment_home, container, false);
+        home = inflater.inflate(R.layout.fragment_home, container, false);
         //private TabLayout tabLayout_main;
         ViewPager2 viewPager2_main = home.findViewById(R.id.main_act_viewPager2);
-        layoutIndicators
+        //layoutIndicators = home.findViewById(R.id.linearLayoutViewPagerIndicators);
 
         assert getArguments() != null;
         int currentPage = getArguments().getInt("StartPage");
@@ -60,9 +65,10 @@ public class Fragment_home_main extends Fragment {
             }
         }
         FragmentManager fragmentManager = getParentFragmentManager();
-        Adapter_HomeActFragment fragmentAdapter = new Adapter_HomeActFragment(fragmentManager, getLifecycle(), user.getUserID());
+        fragmentAdapter = new Adapter_HomeActFragment(fragmentManager, getLifecycle(), user.getUserID());
         viewPager2_main.setAdapter((fragmentAdapter));
 
+        setupIndicators();
         TabLayout tabLayout = home.findViewById(R.id.tab_layout);
         new TabLayoutMediator(tabLayout, viewPager2_main,
                 (tab, position) -> tab.setText("")
