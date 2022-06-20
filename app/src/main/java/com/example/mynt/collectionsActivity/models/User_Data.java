712c6e100f40bfc14165752c26bbd6270408c8dc
JoshMkhari <njmkhari@gmail.com>
2022-06-20 13:59:21 +0200

    Coin details to coin full now working thanks to static User_Data class

---
+++ app/src/main/java/com/example/mynt/collectionsActivity/models/User_Data.java
@@ -0,0 +1,9 @@
+package com.example.mynt.collectionsActivity.models;
+
+import android.graphics.Bitmap;
+
+public class User_Data {// Will Hold static once off data
+
+    public static Model_Coin model_coin;
+    public static Bitmap coinBitmap;
+}
