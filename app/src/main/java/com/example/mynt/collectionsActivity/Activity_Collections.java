6f290f062fcb88c63c509ea46de7725e7378a2af
JoshMkhari <njmkhari@gmail.com>
2022-06-29 16:15:30 +0200

    Modal for coins list

--- app/src/main/java/com/example/mynt/collectionsActivity/Activity_Collections.java
+++ app/src/main/java/com/example/mynt/collectionsActivity/Activity_Collections.java
@@ -47,26 +47,39 @@ public class Activity_Collections extends AppCompatActivity implements Interface
     @Override
     public void onButtonClicked(int buttonID) {
 
-
-        if(Model_User_Data.position==0)
+        if(Model_User_Data.mode==2)
         {
-            Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-            String message = "I used 'Mynt' to store a " + Model_User_Data.model_coin.getValue() + " coin from "
-                    +   Model_User_Data.model_coin.getYear();
-            intent.putExtra(Intent.EXTRA_TEXT,message);
-            startActivity(intent);
-        }
-        if(Model_User_Data.position==1)
+            if(Model_User_Data.position==0)
+            {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String message = "I used 'Mynt' to store a " + Model_User_Data.model_coin.getValue() + " coin from "
+                        +   Model_User_Data.model_coin.getYear();
+                intent.putExtra(Intent.EXTRA_TEXT,message);
+                startActivity(intent);
+            }
+            if(Model_User_Data.position==1)
+            {
+                Bundle bundle = new Bundle();//(valerybodak,2020)
+                bundle.putInt("Task", Model_User_Data.task);
+                bundle.putInt("CoinID", Model_User_Data.model_coin.getCoinID());
+                //ViewCompat.setTransitionName(coinImage, "recentTransaction");
+                findNavController(Objects.requireNonNull(getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2))).navigate(
+                        R.id.action_fragment_home_main_to_fragment_Coin_Details,
+                        bundle);
+            }
+        }else
         {
             Bundle bundle = new Bundle();//(valerybodak,2020)
+
             bundle.putInt("Task", Model_User_Data.task);
             bundle.putInt("CoinID", Model_User_Data.model_coin.getCoinID());
             //ViewCompat.setTransitionName(coinImage, "recentTransaction");
             findNavController(Objects.requireNonNull(getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2))).navigate(
-                    R.id.action_fragment_home_main_to_fragment_Coin_Details,
+                    R.id.action_fragment_Coins_to_fragment_Coin_Details,
                     bundle);
         }
+
 
     }
 }
